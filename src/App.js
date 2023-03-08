78db6721384aad20c6aeafbb9ea27d8bf5121621
Marcos Chinchilla <marcoschinchilla96@gmail.com>
2023-03-07 18:25:24 -0600

    finished responsive desing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, {useState, useEffect, useRef} from "react";
+import SuccesForm from "./components/SuccesForm";
+import "./style/App.css"
 
 function App() {
   const [name, setName] = useState("");
@@ -6,16 +8,16 @@ function App() {
   const [month, setMonth] = useState("");
   const [year, setYear] = useState("");
   const [cvc, setCvc] = useState("");
-  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
 
   const maskNumber = '#### #### #### ####';
   const maskMonth = '##';
   const maskYear = '##';
   const maskCvc = '###';
- 
+
   const focus = useRef(null);
   
-  function handleInput(mask, key, arr){
+  function handleInput(mask, key, arr, input){
     let numbers = ["0","1","2","3","4","5","6","7","8","9"];
 
     if(key == "Backspace" && arr.length > 0){
@@ -27,94 +29,129 @@ function App() {
       }else{
         arr.push(key)
       }
-      isNumber(key)
-    }else if(!numbers.includes(key) && !(key == "Backspace")) {
-      isNumber(key)
+      isNumber(key, input)
     }
+    else if(!numbers.includes(key) && !(key == "Backspace")) {
+      isNumber(key, input)
+    }
+    console.log(key)
   }
 
 
-function handleNumber(e){
-  const temp = [...number]
-  handleInput(maskNumber, e.key, temp)
-  setNumber(temp.join(""))
-}
+  function handleNumber(e){
+    const temp = [...number]
+    handleInput(maskNumber, e.key, temp, e.target)
+    setNumber(temp.join(""))
+  }
 
-function handleMonth(e){
-  const temp = [...month]
-  handleInput(maskMonth, e.key, temp)
-  setMonth(temp.join(""))
-}
-function handleYear(e){
-  const temp = [...year]
-  handleInput(maskYear, e.key, temp)
-  setYear(temp.join(""))
-}
-function handleCvc(e){
-  const temp = [...cvc]
-  handleInput(maskCvc, e.key, temp)
-  setCvc(temp.join(""))
-}
+  function handleMonth(e){
+    const temp = [...month]
+    handleInput(maskMonth, e.key, temp, e.target)
+    setMonth(temp.join(""))
+  }
+  function handleYear(e){
+    const temp = [...year]
+    handleInput(maskYear, e.key, temp, e.target)
+    setYear(temp.join(""))
+  }
+  function handleCvc(e){
+    const temp = [...cvc]
+    handleInput(maskCvc, e.key, temp, e.target)
+    setCvc(temp.join(""))
+  }
 
-
-  function isNumber(key){
-    if(!isNaN(key)){
-      setError(false)
-    }else{
-      setError(true)
+  function isNumber(key, input){
+    if(!isNaN(key) || key == "Tab"){
+      input.nextElementSibling.innerText = ""
     }
-
-
-    // toExamine.forEach(toExam =>{
-    //   const toNumber = toExam.map(Number);
-    //   const exame = toNumber.every(item => !isNaN(item))
-    //   console.log(exame)
-    //   if(exame){setError(true)}
-    // })
-    
+    else{
+      if(input.nextElementSibling.nodeName == "INPUT"){
+        input.nextElementSibling.nextElementSibling.innerText ="Solo numeros"
+      }else{
+        input.nextElementSibling.innerText = "Solo numeros"
+      }
+    }
   }
 
   useEffect(()=> {
     focus.current.focus()
   }, [])
 
-  // function submitValidation(e){
-  //   e.preventDefault();
-  // }
-
-
-
+  function submitValidation(e){
+    e.preventDefault();
+    if(!name){
+      console.log("name is false")
+    }
+    if(!number){
+      console.log("number is false")
+    }
+    if(!month){
+      console.log("month is false")
+    }
+    if(!year){
+      console.log("year is false")
+    }
+    if(!cvc){
+      console.log("cvc is false")
+    }
+    if(name && number && month && year && cvc){
+      setLoading(true)
+    }
+  }
 
 
   return (
     <React.Fragment>
       <section>
-        <article>
-          <p>{name}</p>
-          <p>{number}</p>
-          <p>{month}</p>
-          <p>{year}</p>
+        <article className="front">
+        <div className="icon_container">
+          <svg width="84" height="47" viewBox="0 0 84 47" fill="none" xmlns="http://www.w3.org/2000/svg"><ellipse cx="23.478" cy="23.5" rx="23.478" ry="23.5" fill="#fff"/><path d="M83.5 23.5c0 5.565-4.507 10.075-10.065 10.075-5.559 0-10.065-4.51-10.065-10.075 0-5.565 4.506-10.075 10.065-10.075 5.558 0 10.065 4.51 10.065 10.075Z" stroke="#fff"/></svg>
+        </div>
+          
+          <div className="text-container">
+            <p className="number">{number}</p>
+            <div className="text-container--data">
+              <p className="name">{name}</p>
+              <div>
+                <p className="month">{month}/</p>
+                <p className="year">{year}</p>
+              </div>
+            </div>
+          </div>
+          
         </article>
-        <article>
+        <article className="back">
           <p>{cvc}</p>
         </article>
       </section>
       <main>
-        <form >
+        {loading ? <SuccesForm/> : <form >
           <label>CARDHOLDER NAME</label>
-          <input type="text" value={name}  ref={focus} onChange={(e) =>{ setName(e.target.value)}}/>
+          <input placeholder="e.g. Richard Tapia" type="text" value={name}  ref={focus} onChange={(e) =>{ setName(e.target.value)}}/>
+
           <label>CARD NUMBER</label>
-          <input type="text"  value={number} onKeyDown={handleNumber}/>
-          <p>{error ? "solo numeros" : ""}</p>
-          <label>EXP. DATE</label>
-          <input type="text" value={month} onKeyDown={handleMonth}/>
-          <input type="text" value={year} onKeyDown={handleYear }/>
-          <p>{error ? "solo numeros" : ""}</p>
-          <label>CVC</label>
-          <input type="text" value={cvc} onKeyDown={handleCvc}/>
-          <input type="submit" value="Confirm"/>
-          <p>{error ? "solo numeros" : ""}</p>
-        </form>
+          <input placeholder="e.g. 1234 5678 9123 0000" type="text" value={number} onKeyDown={handleNumber}/>
+          <p ></p>
+
+          <div className="input_container">
+            <div className="input_container--date">
+            <label>EXP. DATE</label>
+              <div>
+              <input placeholder="MM" type="text" value={month} onKeyDown={handleMonth}/>
+              <input placeholder="YY" type="text" value={year} onKeyDown={handleYear }/>
+              <p></p>
+              </div>
+            </div>
+
+            <div className="input_container--cvc">
+              <label>CVC</label>
+              <input placeholder="e.g. 123" type="text" value={cvc} onKeyDown={handleCvc}/>
+              <p ></p>
+            </div>
+          </div>
+
+        </form>}
+        <input type="submit" onClick={submitValidation} value={loading ? "Continue" : "Confirm"}/>
       </main>
     </React.Fragment>
   );
---
+++ src/components/SuccesForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../style/succesForm.css"
+function SuccesForm() {
+    return(
+        <div className='succes_container'>
+            <svg width="80" height="80" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="40" cy="40" r="40" fill="url(#a)"/><path d="M28 39.92 36.08 48l16-16" stroke="#fff" strokeWidth="3"/><defs><linearGradient id="a" x1="-23.014" y1="11.507" x2="0" y2="91.507" gradientUnits="userSpaceOnUse"><stop stopColor="#6348FE"/><stop offset="1" stopColor="#610595"/></linearGradient></defs></svg>
+            <h1>Thank You!</h1>
+            <p>we've added your cart details</p>
+        </div>
+    )
+}
+
+export default SuccesForm;
